80ac9bde87f5ae27b2527429c31dcf73342eb407
abhinavnist <abhinavpatna55@gmail.com>
2023-12-21 23:45:00 +0530

    operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -11,14 +11,44 @@ console.log(valueInNumber)*/
 // "33abc" => NaN
 // true => 1; false => 0
 
-let isLoggedIn = 1
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+// let isLoggedIn = 1
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
 // 1 => true, 0 => false
 // "" => false
 // "hitesh" => true
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber) //number can be converted into the string
+// console.log(stringNumber)
+// console.log(typeof stringNumber) //number can be converted into the string
+
+//**************operations ********************* */
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+// console.log(2 + 2)
+// console.log(2 - 2)
+// console.log(2 * 2)
+// console.log(2 / 2)
+// console.log(2 % 2)
+
+let str1 = "hello"
+let str2 = "abhinav"
+let str3 = str1 + str2
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "2")
+// console.log(3 + ((4 * 5) % 2)) not do like this use bracket for redable
+// console.log(true)
+// console.log(+true) =>1
+// console.log(true+) =>error
+// console.log(+"") => 0
+
+// let num1, num2, num3
+// num1 = num2 = num3 = 2 + 2 //not write like this
+let gameCounter = 100
+++gameCounter
+console.log(gameCounter)
