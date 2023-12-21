c4c6d79c2d5aba8f5e806ac28bcd3ffe73b21778
abhinavnist <abhinavpatna55@gmail.com>
2023-12-21 23:51:26 +0530

    comparision what not to do

---
+++ 01_basics/04_comparasion.js
@@ -0,0 +1,18 @@
+// console.log(2 > 1)
+// console.log(2 >= 1)
+// console.log(2 < 1)
+// console.log(2 == 1)
+
+// console.log("2" > 1) //(true) not same datatype it change into the integer
+// console.log("02" > 1) //also give true
+
+//this type of conversion we dont do redablity low
+// console.log(null > 0) //=> false
+// console.log(null == 0)=> false
+// console.log(null >= 0) => true
+// console.log(undefined > 0) //=> false
+// console.log(undefined == 0) //=> false
+// console.log(undefined >= 0) //=> false
+
+// === strict check
+// console.log("2" === 2) //strict check it also check the datatype
